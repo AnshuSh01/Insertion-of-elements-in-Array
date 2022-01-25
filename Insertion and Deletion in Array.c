5135795cfffed27490555d39039c0582eab140fb
Anshu Sharma <95487059+AnshuSh01@users.noreply.github.com>
2022-01-25 14:41:04 +0530

    Update and rename Insertion in Array.c to Insertion and Deletion in Array.c

--- Insertion and Deletion in Array.c
+++ Insertion and Deletion in Array.c
@@ -11,6 +11,21 @@ void insertion(int a[],int Number,int index,int size)
     a[index]=Number;
      
 } 
+void delete(int a[],int index,int size)
+{   int store_index;
+    int store_element;
+     
+    store_element=a[index];
+    store_index=index;
+    while(store_index<size-1)
+    {
+        a[store_index]=a[store_index+1];
+        store_index++;
+    }
+    size=size-1;   
+    
+    printf("Deleted Element %d from index %d ",store_element,index);
+}
 
 int main()
 {   int size;
@@ -33,7 +48,7 @@ int main()
         printf("%d\t",a[i]);
     }
     while(1)
-    {    printf("\nenter 1 For insertion and 0 for Exit\t");
+    {    printf("\nenter 1 For insertion 2 for delete and 0 for Exit\t");
          scanf("%d",&choice);
     
         switch(choice)
@@ -52,6 +67,20 @@ int main()
                       printf("%d\t",a[i]);
                        }
                 }
+                case 2:
+                {    
+                    printf("\nEnter index at which U want to delete the element\n");
+                    scanf("%d",&index);
+                    delete(a,index,size);
+                    printf("elements are=\n");
+                    size=size-1;
+                    for(int i=0;i< size;i++)
+                {
+                      printf("%d\t",a[i]);
+                       }
+                }
+                    
+                
                 case 0:
                 {
                     exit(0);
@@ -59,6 +88,5 @@ int main()
                 
             
              }
-    
 }
 }
